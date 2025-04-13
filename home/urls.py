0ddd13380553d2ff69b9000e4edc08a2b4676463
Kiwindonix <vitia.vasylenko@gmail.com>
2025-04-13 13:18:01 +0300

    .

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from django.http import HttpResponse
 
 urlpatterns = [
     path('', views.home_view, name='home'),
-    
+    path('news/', views.news_view, name='news'),
+    path('profile/', lambda request: HttpResponse("Профіль (тимчасово)"), name='profile'),
+    path('calendar/', lambda request: HttpResponse("Календар (тимчасово)"), name='calendar_redirect'),
+    path('gallery/', lambda request: HttpResponse("Галерея (тимчасово)"), name='gallery'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, 'home/home.html')  # Шаблон буде в home/templates/home/
+
+def news_view(request):
+    return render(request, 'news.html')
